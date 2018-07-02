19f988d60184806d6d239d834dae0399d7ef8fba
mivihor <igor.mykula@gmail.com>
2018-07-02 23:43:22 +0300

    Business Logic Layer -> Infrastructure/ValidationException.cs Business Logic Layer -> Interfaces/IGetUser.cs

---
+++ GamesProject/GamesProject.BusinessLogicLayer/Infrastructure/ValidationException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesProject.BusinessLogicLayer.Infrastructure
+{
+    class ValidationException
+    {
+    }
+}
---
+++ GamesProject/GamesProject.BusinessLogicLayer/Interfaces/IGetUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesProject.BusinessLogicLayer.Interfaces
+{
+    interface IGetUser
+    {
+    }
+}

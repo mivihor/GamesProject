569e602189553559694cff04a39850c73ca4c7fc
mivihor <igor.mykula@gmail.com>
2018-07-30 19:23:32 +0300

    access token modifie (worked)

--- GamesProject.Client/GPClient/src/app/signInComponent/signIn.component.ts
+++ GamesProject.Client/GPClient/src/app/signInComponent/signIn.component.ts
@@ -4,6 +4,8 @@ import { Observable } from "rxjs";
 import { userService } from "./user.service";
 import { userModel } from "./userModel";
 import { HttpErrorResponse } from "../../../node_modules/@angular/common/http";
+import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
+
 
 @Component({
     templateUrl: './signIn.component.html',
@@ -13,12 +15,14 @@ export class SignInComponent{
 user:userModel = new userModel();
 isLoginError:boolean = false;
 
-constructor(private uservice: userService){}
+constructor(private uservice: userService){
+}
 
 Submit(ucred:userModel){
-    alert(`${ucred.Login}-----${ucred.Password}`);
+    //alert(`${ucred.Login}-----${ucred.Password}`);
+    
     this.uservice.userAuth(ucred.Login,ucred.Password).subscribe((data:any)=>{
-        localStorage.setItem('userToken',data.userToken);
+        localStorage.setItem('userToken',data.token);    
         //router navigate ==> home
 },
     (err:HttpErrorResponse)=>{
--- GamesProject.Client/GPClient/src/app/signInComponent/user.service.ts
+++ GamesProject.Client/GPClient/src/app/signInComponent/user.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {  Response } from "@angular/http";
 import {Observable} from 'rxjs';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
 @Injectable({
     providedIn: 'root'
